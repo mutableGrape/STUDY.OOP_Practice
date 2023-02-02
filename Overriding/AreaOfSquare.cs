4caee37036f361e6120705b77fba2a30a86a78b7
mutableGrape <danielworton@live.co.uk>
2023-02-02 15:45:37 +0000

    Lesson 6 Polymorphism: Overriding complete

---
+++ Overriding/AreaOfSquare.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overriding
+{
+    class AreaOfSquare
+    {
+        public virtual double Square(double a, double b)
+        {
+            Console.WriteLine("Calculate area of square power a^b");
+            return Math.Pow(a, b);
+        }
+    }
+}
---
+++ Overriding/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AreaOfShapes areaSquare = new AreaOfShapes();
+            double a = 10.00;
+            double b = 10.00;
+            double area = 0.0;
+            area = areaSquare.Square(a, b);
+            Console.WriteLine("area of square: {0}", area);
+            Console.ReadLine();
+        }
+    }
+}
