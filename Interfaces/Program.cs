db986ba3ea99daece26bd4b5cfc17292658d207a
mutableGrape <danielworton@live.co.uk>
2023-02-02 15:19:30 +0000

    Lesson 4: Interfaces complete

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace Interfaces
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Rectangle rec = new Rectangle();
+            Circle circle = new Circle();
+
+            double a = 10;
+            double b = 20;
+            double c = 1.0;
+            double area = 0.0;
+            double perimeter = 0.0;
+
+            rec.height = a;
+            rec.width = b;
+            area = rec.Area();
+            Console.WriteLine("Area of rectangle is : {0:F}", area);
+            perimeter = rec.Perimeter();
+            Console.WriteLine("Perimeter of rectangle is : {0:F}", perimeter);
+
+            Console.WriteLine("---------------------------");
+
+            circle.radius = c;
+            area = circle.Area();
+            Console.WriteLine("Area of circle is : {0:F}", area);
+            perimeter = circle.Perimeter();
+            Console.WriteLine("Perimeter of circle is : {0:F}", perimeter);
+
+            Console.ReadLine();
+
         }
     }
 }
